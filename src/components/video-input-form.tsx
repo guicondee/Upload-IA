abdb9ba9c5593f2942edf8742364f3b9c2b804c6
Guilherme vinicios conde <guicondee@outlook.com>
2024-05-28 15:59:15 -0300

    função para converter video em audio criada

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -4,6 +4,8 @@ import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 export function VideoInpuForm() {
   const [videoFile, setVideoFile] = useState<File | null>(null);
@@ -19,7 +21,46 @@ export function VideoInpuForm() {
     setVideoFile(selectedFile);
   }
 
-  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+  async function convertVideoToAudio(video: File) {
+    console.log("convert started");
+
+    const ffmpeg = await getFFmpeg();
+
+    await ffmpeg.writeFile("input.mp4", await fetchFile(video));
+
+    // ffmpeg.on("log", (log) => {
+    //   console.log(log);
+    // });
+
+    ffmpeg.on("progress", (progress) => {
+      console.log("Convert progress:" + Math.round(progress.progress * 100));
+    });
+
+    await ffmpeg.exec([
+      "-i",
+      "input.mp4",
+      "-map",
+      "0:a",
+      "-b:a",
+      "20k",
+      "-acodec",
+      "libmp3lame",
+      "output.mp3",
+    ]);
+
+    const data = await ffmpeg.readFile("output.mp3");
+
+    const audioFileBlob = new Blob([data], { type: "audio/mpeg" });
+    const audioFile = new File([audioFileBlob], "audio.mp3", {
+      type: "audio/mpeg",
+    });
+
+    console.log("Convert finished");
+
+    return audioFile;
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const prompt = promptInputRef.current?.value;
@@ -29,6 +70,9 @@ export function VideoInpuForm() {
     }
 
     //converter o video em mp3
+
+    const audioFile = await convertVideoToAudio(videoFile);
+    console.log(audioFile);
   }
 
   const previewURL = useMemo(() => {
